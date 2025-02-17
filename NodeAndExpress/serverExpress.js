dcd095b5ced6dfd1e7d3f04823b978e8952c4404
Joona Nuorala <joona.nuorala@gmail.com>
2025-02-17 19:37:37 +0200

    updated serverExpress.js

--- NodeAndExpress/serverExpress.js
+++ NodeAndExpress/serverExpress.js
@@ -7,5 +7,5 @@ app.use(express.static(path.join(__dirname)));
 
 const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(`Palvelin käynnissä osoitteessa http://localhost:${PORT}`);
+  console.log(`Server running at http://192.168.126.168:${PORT}`);
 });
